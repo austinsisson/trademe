ea7a2b4fa016c8bd6bed5b5ad7416e05dffe38ee
Austin Sisson <austinsisson@gmail.com>
2015-04-07 03:22:01 +0000

    updated shift layout on workplace show and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @user_workplaces = @user.user_workplaces.all
+    @user_shifts = Shift.where(user_name: current_user.name)
   end
   
   def update
